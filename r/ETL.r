75e3bb21c3fcc8f643a4a7a08487682aad15f29e
root <twistedogic@gmail.com>
2014-09-11 11:07:52 +0000

    update r

--- r/ETL.r
+++ r/ETL.r
@@ -2,5 +2,19 @@ library(quantmod)
 library(lubridate)
 library(TTR)
 library(rredis)
+library(tools)
 # parameters
-allStock <- list.files(path='chronoJob/node/dataset')
+setwd('/home/myworkspace/chronoJob/node/dataset/')
+stockId <- list.files(path='.')
+for (i in 1:length(stockId)){
+#tmp <- write.zoo(read.csv(file=stockId[i],sep=','),file=stockId[i],sep=',')
+stockId[i] <- file_path_sans_ext(stockId[i])
+}
+endDate <- Sys.Date()
+d <- day(endDate)
+m <- month(endDate)
+y <- year(endDate) - 2
+startDate <- paste(y,m,d,sep="-")
+startDate <- as.Date(startDate)
+getSymbols(stockId,src='csv', to = endDate,return.class='xts')
+setwd('/home/myworkspace/')
