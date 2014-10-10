a55c7718e26c413bb1cc210565b818804ce3b5e4
root <twistedogic@gmail.com>
2014-10-10 18:27:08 +0000

    update ML

--- r/modules/ml.r
+++ r/modules/ml.r
@@ -1,11 +1,20 @@
 library(h2o)
+path = getwd()
 localH2O = h2o.init(ip = "localhost", port = 54321)
-test.hex = h2o.importFile(localH2O, path = 'report/report.csv', key = "test.hex")
-stock.hex = h2o.importFile(localH2O, path = 'report/results/0001.HK.csv', key = "stock.hex")
+test.hex = h2o.importFile(localH2O, path = paste(path,'/report/report.csv',sep=''), key = "test.hex")
 test.km = h2o.kmeans(data = test.hex, centers = 2, cols = tatitle)
-stock.km = h2o.kmeans(data = stock.hex, centers = 2, cols = tatitle[6:46])
-stock.train = stock.hex[1:3000]
-stock.test = stock.hex[3001:nrow(stock.hex)]
-stock.glm = h2o.glm(y = "Close", x = tatitle[6:46], data = stock.train, family = "gaussian", nfolds = 10, alpha = 0.5)
-stock.fit = h2o.predict(object=stock.glm,newdata=stock.test)
-proto <- cbind(as.matrix(stock.fit[,1]),as.matrix(stock.test[,5]))
+proto = character();
+for(i in 1:length(stockId)){
+	stock = stockId[i]
+	print(stock)
+	stock.hex = h2o.importFile(localH2O, path = paste(path,'/report/results/',stock,'.csv',sep=''), key = "stock.hex")
+	train = ceiling(nrow(stock.hex)/2)
+	stock.km = h2o.kmeans(data = stock.hex, centers = 2, cols = tatitle[6:46])
+	stock.train = stock.hex[index(stock.hex) < train]
+	stock.test = stock.hex[index(stock.hex) > train]
+	stock.glm = h2o.glm(y = "fclose", x = tatitle[1:47], data = stock.train, family = "gaussian", nfolds = 10, alpha = 0.5)
+	stock.fit = h2o.predict(object=stock.glm,newdata=stock.test[,1:47])
+	temp = cbind(as.matrix(stock.fit[,1]),as.matrix(stock.test[,48]))
+	t = (temp[,1] - temp[,2])*100/temp[,2]
+	proto = rbind(proto,mean(t))
+}
