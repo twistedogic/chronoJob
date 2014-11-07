e11688680088d15a9e59c4617a474df84776542e
root <twistedogic@gmail.com>
2014-11-07 10:58:47 +0000

    update yql

--- node/news.js
+++ node/news.js
@@ -1,20 +1,18 @@
 var fs = require('fs');
-var request = require('request');
+var YQL = require('yql');
+
 var stockIds = fs.readdirSync(__dirname + '/dataset');
 for(var i = 0; i < stockIds.length; i++){
     stockIds[i] = stockIds[i].split('.')[0] + '.' + stockIds[i].split('.')[1];
 }
-// for (var i = 0; i < stockIds.length; i++){
-    var url = 'http://query.yahooapis.com/v1/public/yql?q=select+title%2Clink%2CpubDate%2Cdescription+from+rss+where+url+%3D+%22http%3A%2F%2Ffeeds.finance.yahoo.com%2Frss%2F2.0%2Fheadline%3Fs%3D' + stockIds[0] + '%26region%3DUS%26lang%3Den-US%22&format=json';
-    request({
-      url: url
-    }, function (err, res, body) {
-      if (err) { return cb(err); }
-      if (res.statusCode == 200){
-        console.log(body);
-      } else {
-        console.log('fail to download '+ stockIds[i]);
-      }
+console.log(stockIds.length);
+for (var i = 1; i < stockIds.length; i++){
+    var query = new YQL('select pubDate,title,link,description from rss where url="http://finance.yahoo.com/rss/industry?s='+ stockIds[i] +'"');
+    query.exec(function (error, response) {
+        if(!error){
+            console.log(response.query.results);
+        }
+        // Do something with results (response.query.results)
     });
-// }
+}
 console.log('Scraping...');
