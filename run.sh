9358b7293192e86d23fd9d0eb26cc0479a451607
root <twistedogic@gmail.com>
2014-11-26 00:30:07 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 location=$(pwd)
-rm -rf ${location}/report/*.csv
-rm -rf ${location}/report/results/*.csv
-rm -rf ${location}/node/lookupTable.r
-cd ${location}/node/ && npm install
-cd ${location}/node/dataset/ && rm -rf *
-cd ${location}/node && node app.js
-#cd ${location} && Rscript r/main.r
-#cd ${location}/node && node report.js
+rm -rf ${location}/chronoJob/report/*.csv
+rm -rf ${location}/chronoJob/report/results/*.csv
+rm -rf ${location}/chronoJob/node/lookupTable.r
+cd ${location}/chronoJob/node/ && npm install
+cd ${location}/chronoJob/node/dataset/ && rm -rf *
+cd ${location}/chronoJob/node && node app.js
+Rscript chronoJob/r/main.r
+#cd ${location}/chronoJob/node && node report.js
