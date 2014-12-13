226549e341211e356c29870eb4031ad71fdde1c3
root <twistedogic@gmail>
2014-12-13 16:45:04 +0000

    update drill query

--- node/drill.js
+++ node/drill.js
@@ -4,17 +4,31 @@ var xpath = require('xpath');
 var dom = require('xmldom').DOMParser;
 var formData = {
     queryType:'SQL',
-    query:'select * from dfs.`/data/info/00001BalanceSheet-AnnualResults.csv`'
+    query:'SELECT * FROM cp.`employee.json` LIMIT 20'
 };
 var data = qs.stringify(formData);
 
 request.post({url:'http://192.168.100.74:8047/query', form: data}, function optionalCallback(err, httpResponse, body) {
   if (err) {
-    return console.error('upload failed:', err);
+    return console.error('query failed:', err);
   }
   var doc = new dom().parseFromString(body);
   var nodes = xpath.select('//td', doc);
-  for (var i = 0; i < nodes.length; i++){
-      console.log(nodes[i].firstChild.data);
+  var title = xpath.select('//th', doc);
+  console.log(title[0].firstChild.data);
+  console.log(title.length);
+  console.log(nodes.length);
+  var nrow = nodes.length/title.length;
+  var csv = title[0].firstChild.data;
+  for (var i = 1; i < title.length; i++){
+      csv = csv + ',' + title[i].firstChild.data;
   }
+  for (var k = 0; k < nodes.length; k += title.length){
+    csv = csv + '\n' + nodes[k].firstChild.data;
+    console.log(k);
+    for (var l = 1; l < title.length; l++){
+        csv = csv + ',' + nodes[k + l].firstChild.data;
+    }
+  }
+  console.log(csv);
 });
