76f2cb27e924d81fe693b3f50969b3a219439a20
root <twistedogic@gmail.com>
2015-03-03 05:54:11 +0000

    update query

--- node/test.js
+++ node/test.js
@@ -84,9 +84,87 @@ db.get('00001'+ options[5]).then(function(res){
 });
 
 db.query('companyinfo2/' + options[0]).then(function (res) {
-    var keys = Object.keys(res);
-    console.log(keys);
-    // console.log(res.rows);
+    var all = res.rows;
+    var data = JSON.parse(all[1].key);
+    data[0] = data[data.length - 2];
+    data[1] = data[data.length - 1];
+	if (data[0].cover_period > data[1].cover_period){
+	    var full = Object.keys(data[0]);
+	    var half = Object.keys(data[1]);
+	    for (var j = 0; j < full.length; j++) {
+	        var json = data[0];
+	        if (typeof json[full[j]] === 'string'){
+	            full.move(j,0);
+	        }
+	    }
+	    for (var j = 0; j < half.length; j++) {
+	        var json = data[1];
+	        if (typeof json[half[j]] === 'string'){
+	            half.move(j,0);
+	        }
+	    }
+	} else {
+	    var half = Object.keys(data[0]);
+	    var full = Object.keys(data[1]);
+	    for (var j = 0; j < full.length; j++) {
+	        var json = data[1];
+	        if (typeof json[full[j]] === 'string'){
+	            full.move(j,0);
+	        }
+	    }
+	    for (var j = 0; j < half.length; j++) {
+	        var json = data[0];
+	        if (typeof json[half[j]] === 'string'){
+	            half.move(j,0);
+	        }
+	    }
+	}
+	var fullcsv = full;
+	var halfcsv = half;
+	for (var n = 0; n < all.length; n++) {
+	    var data = JSON.parse(all[n].key);
+	    for (var j = 0; j < data.length; j++) {
+	        var json = data[j];
+	        var line = [];
+	        if (json.cover_period > 6){
+	            for (var k = 0; k < full.length; k++) {
+	                line.push(json[full[k]])
+	            }
+	            fullcsv = fullcsv + '\n' + line.join(',');
+	        } else {
+	            for (var k = 0; k < half.length; k++) {
+	                line.push(json[half[k]])
+	            }
+	            halfcsv = halfcsv + '\n' + line.join(',');
+	        }
+	    }
+    }
+    // console.log(halfcsv);
+    // console.log(fullcsv);
 }).catch(function (err) {
     console.log(err);
+});
+
+db.query('companyinfo2/'+ options[5]).then(function(res){
+    // console.log(res);
+    var all = res.rows;
+    var data = JSON.parse(all[1].key);
+    var keys = Object.keys(data);
+    for (var j = 0; j < keys.length; j++) {
+        var json = data;
+        if (typeof json[keys[j]] === 'string'){
+            keys.move(j,0);
+        }
+    }
+    var csv = keys.join(',');
+    for (var i = 0; i < all.length; i++) {
+        var data = JSON.parse(all[i].key);
+        var values = [];
+        for (var m = 0; m < keys.length; m++) {
+            values.push(data[keys[m]]);
+        }
+        csv = csv + '\n' + values.join(',');
+    }
+    console.log(csv);
+    fs.writeFileSync(__dirname + '/dataset/fundamentals.csv',csv);
 });
