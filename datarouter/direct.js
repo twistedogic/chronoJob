12753a21cbecc8636c8eb019e1c83c5ae5a99b85
root <twistedogic@gmail.com>
2015-03-05 07:04:42 +0000

    update datarouter

---
+++ datarouter/direct.js
@@ -0,0 +1,121 @@
+var express = require('express');        // call express
+var app = express();                 // define our app using express
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var request = require('request');
+var moment = require('moment');
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ 
+    extended: true,
+    limit: '50mb'
+}));
+app.use(bodyParser.json({limit: '50mb'}));
+
+//logger
+app.use(morgan('tiny'));
+
+var port = process.env.OPENSHIFT_INTERNAL_PORT || process.env.OPENSHIFT_NODEJS_PORT || 3001; // set our port
+var ip = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+app.get('/', function(req, res) {
+    res.json({message:'Hi'})
+})
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.get('/api/hist/:stockId', function(req, res) {
+    var stockId = req.params.stockId;
+    var url = 'http://finance.yahoo.com/_td_charts_api/resource/charts;gmtz=8;indicators=quote;range=2y;rangeSelected=undefined;ticker=' + stockId;
+    request({
+        url: url
+    }, function (err, resp, body) {
+        if (err) { return cb(err); }
+        if (resp.statusCode == 200){
+            var body = JSON.parse(body);
+            var data = body.data;
+            var symbol = data.meta.symbol;
+            var time = data.timestamp;
+            var ohlc = data.indicators.quote[0];
+            var open = ohlc.open;
+            var high = ohlc.high;
+            var low = ohlc.low;
+            var close = ohlc.close;
+            var volume = ohlc.volume;
+            console.log(symbol);
+            var output = 'Symbol,Date,Open,High,Low,Close,Volume';
+            for (var j = 0; j < time.length; j++){
+                if (open[j] !== null && high[j] !== null && low[j] !== null && close[j] !== null && volume[j] !== null){
+                    output = output + '\n' + symbol + ',' + moment.unix(time[j]).zone('+0800').format("YYYY-MM-DD") + ',' + open[j] + ',' + high[j] + ',' + low[j] + ',' + close[j] + ',' + volume[j];
+                }
+            }
+            res.contentType('csv');
+            res.send(output);
+        } else {
+            res.json({message:'fail to download '+ stockId});
+        }
+    });
+});
+
+app.get('/api/ta/r/:stockId', function(req, res) {
+    
+    var stockId = req.params.stockId;
+    request.post({
+        url:'http://twistedogic.ocpu.io/TAA/R/TAA', 
+        json: {"id":stockId,"endPoint":"http://api-twistedogic01.rhcloud.com/api/hist/"}
+    }, function(err,resp,body){
+        if(err){
+            res.json({message:'record not found'});
+            console.log(err);
+        } else {
+            var data = body.split('\n');
+            var check = body.split('ocpu').length;
+            if(check > 1){
+                var dataurl = resp.headers.location + 'R/.val/csv';
+                console.log(dataurl);
+                request(dataurl,function(err,resp,body){
+                    if(err){
+                        console.log(err);
+                        res.json({message:'R error'});
+                    } else {
+                        var csv = body.split('\n');
+                        var header = csv[0].split('coredata.');
+                        csv[0] = header.join('');
+                        csv = csv.join('\n');
+                        res.contentType('csv');
+                        res.send(csv);
+                    }
+                })
+            } else {
+                res.json({message:'R error'});
+            }
+        }
+    }) 
+});
+
+app.get('/api/sector/:sector', function(req, res) {
+    var sector = req.params.sector;
+    db.get(sector,function(err,resp){
+        var data = resp;
+    })
+});
+
+app.get('/api/news/tc/:stockId', function(req, res) {
+    res.json({message:'coming soon'});
+});
+app.get('/api/news/en/:stockId', function(req, res) {
+    res.json({message:'coming soon'});
+});
+app.get('/api/info/:stockId', function(req, res) {
+    res.json({message:'coming soon'});
+});
+
+// START THE SERVER
+// =============================================================================
+app.listen(port,ip);
+console.log("server listening at " + port);
---
+++ node/cache.js
@@ -0,0 +1,66 @@
+var fs = require('fs');
+var request = require('request');
+var PouchDB = require('pouchdb');
+var db = new PouchDB('companyinfo');
+var fileUrl = process.argv[3] || 'https://raw.githubusercontent.com/twistedogic/chronoJob/master/bluechip';
+
+function pad(n, width, z) {
+  z = z || '0';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
+request(fileUrl,function(err,res,body){
+    if (!err){
+        fs.writeFileSync(__dirname + '/target', body);
+    }
+});
+
+setTimeout(function(){
+	var allStock = fs.readFileSync(__dirname + '/target','utf8');
+	var lines = allStock.split('\n');
+	var stockIds = [];
+	for (var i = 0; i < lines.length; i++){
+		var stock = lines[i].split('_')[0].split('.')[0];
+		stock = pad(stock,5);
+		stockIds.push(stock);
+	}
+// 	console.log(stockIds[0]);
+// 	var stockIds = ['00001']; //test
+	var baseurl = 'https://api.investtab.com/api/quote/';
+	var options = [':HK/info'];
+	for (var i = stockIds.length - 1; i >= 0; i--) {
+		for (var j = options.length - 1; j >= 0; j--) {
+			var url = baseurl + stockIds[i] + options[j];
+			request(url,function(err,res,body){
+			    if(!err){
+			        var data = JSON.parse(body);
+			        var key = [];
+			        var value = [];
+			        var info = ['sector', 'industry', 'sub_industry'];
+			        for (var k = 0; k < info.length; k++) {
+			            var json = data[info[k]];
+			            key.push(json.name.en.replace(/(,|\n|\m| | )/g, ''));
+			            var list = json.symbols;
+			            for (var m = 0; m < list.length; m++) {
+			                var tick = list[m].split(':')
+			                tick[0] = tick[0].substring(1);
+			                tick = tick.join('.')
+			                list[m] = tick;
+			            }
+			            value.push(list);
+			        }
+			        for (var k = 0; k < key.length; k++) {
+			            var doc = {
+			                _id: key[k],
+			                symbols: value[k]
+			            };
+			            db.put(doc).then(function(res){
+			                console.log(res);
+			            })
+			        }
+			    }
+			});
+		};
+	};
+},3000);
