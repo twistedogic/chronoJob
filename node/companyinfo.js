97e78145877bdbd7972d25f40c4f4c647aca40ef
root <twistedogic@gmail.com>
2015-04-14 10:56:00 +0000

    remove news

--- node/companyinfo.js
+++ node/companyinfo.js
@@ -1,8 +1,7 @@
 var fs = require('fs');
 var request = require('request');
-var redis = require('redis');
 var PouchDB = require('pouchdb');
-var db = new PouchDB('companyinfo');
+var db = new PouchDB('test/companyinfo');
 // var redisHost = process.argv[2] || '172.17.8.101';
 // var client = redis.createClient(6379, redisHost, {})
 var fileUrl = process.argv[2] || 'https://raw.githubusercontent.com/twistedogic/chronoJob/master/list/bluechip';
@@ -15,49 +14,47 @@ function pad(n, width, z) {
 
 request(fileUrl,function(err,res,body){
     if (!err){
-        fs.writeFileSync(__dirname + '/target', body);
+        var allStock = body;
+        var lines = allStock.split('\n');
+        var stockIds = [];
+        for (var i = 0; i < lines.length; i++){
+            var stock = lines[i].split('_')[0].split('.')[0];
+            stock = pad(stock,5);
+            stockIds.push(stock);
+        }
+//      console.log(stockIds[0]);
+//      var stockIds = ['00001']; //test
+        var baseurl = 'https://api.investtab.com/api/quote/';
+        var options = [
+        ':HK/financial-ratios',
+        ':HK/balance-sheet',
+        ':HK/cashflow-statement',
+        ':HK/income-statement',
+        ':HK/earnings-summary',
+        ':HK/dividend-history',
+        ':HK/fundamentals'
+//      ':HK/info'
+        ];
+        for (var i = stockIds.length - 1; i >= 0; i--) {
+            for (var j = options.length - 1; j >= 0; j--) {
+                var url = baseurl + stockIds[i] + options[j];
+                request(url,function(err,res,body){
+                    if(!err){
+                        var name = res.req.path;
+                        name = name.split('/');
+                        var key = name[3].split(':')[0] + name[4];
+                        var doc = {
+                            _id:key,
+                            data: body
+                        };
+                        db.put(doc).then(function(response) {
+                            console.log(response);
+                        })
+                    }
+                });
+            };
+        };
     }
 });
 
-setTimeout(function(){
-	var allStock = fs.readFileSync(__dirname + '/target','utf8');
-	var lines = allStock.split('\n');
-	var stockIds = [];
-	for (var i = 0; i < lines.length; i++){
-		var stock = lines[i].split('_')[0].split('.')[0];
-		stock = pad(stock,5);
-		stockIds.push(stock);
-	}
-// 	console.log(stockIds[0]);
-// 	var stockIds = ['00001']; //test
-	var baseurl = 'https://api.investtab.com/api/quote/';
-	var options = [
-	':HK/financial-ratios',
-	':HK/balance-sheet',
-	':HK/cashflow-statement',
-	':HK/income-statement',
-	':HK/earnings-summary',
-	':HK/dividend-history',
-	':HK/fundamentals'
-// 	':HK/info'
-	];
-	for (var i = stockIds.length - 1; i >= 0; i--) {
-		for (var j = options.length - 1; j >= 0; j--) {
-			var url = baseurl + stockIds[i] + options[j];
-			request(url,function(err,res,body){
-			    if(!err){
-			        var name = res.req.path;
-			        name = name.split('/');
-			        var key = name[3].split(':')[0] + name[4];
-			        var doc = {
-			            _id:key,
-			            data: body
-			        };
-			        db.put(doc).then(function(response) {
-			            console.log(response);
-			        })
-			    }
-			});
-		};
-	};
-},3000);
+
