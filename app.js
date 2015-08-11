f746a3eab16e57468ade1b71fb1a6885ff0f9f7c
root <root@84cb15e7c815.(none)>
2015-08-11 08:04:50 +0000

    add csv2json & options modules

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var connect = require('connect'),
     directory = __dirname + '/public';
 var port = process.ENV.port || '3001';
-var bootstrap = require('./index.js');
+var bootstrap = require('./lib/files.js');
 
 connect()
     .use(connect.static(directory))
