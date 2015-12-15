28732ea2419f65186625f69f32bb8248731f5344
root <twistedogic@gmail.com>
2015-12-15 14:57:23 +0000

    add coverage automation

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,26 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    mocha = require('gulp-mocha');
+	gutil = require('gulp-util'),
+	mocha = require('gulp-mocha'),
+  istanbul = require('gulp-istanbul');
 
-gulp.task('test',function(){
-    return gulp.src('./test/*.js',{read:false})
-    // .pipe(mocha({reporter: 'nyan'}))
-    .pipe(mocha())
-    .on('error', gutil.log);
+gulp.task('pre-test', function () {
+  return gulp.src(['lib/**/*.js'])
+    // Covering files
+    .pipe(istanbul())
+    // Force `require` to return covered files
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test',['pre-test'],function(){
+	return gulp.src('./test/*.js',{read:false})
+	// .pipe(mocha({reporter: 'nyan'}))
+	.pipe(mocha())
+	.on('error', gutil.log)
+  .pipe(istanbul.writeReports());
 })
 
 gulp.task('watch',function(){
-    gulp.watch(['./lib/**/*.js'],['test'])
+	gulp.watch(['./lib/**/*.js'],['test'])
 })
 
 gulp.task('default',['test']);
